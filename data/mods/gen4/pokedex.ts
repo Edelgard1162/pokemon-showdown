362182e2376dc704cfd1e1094adf4a1f157d65e3
Edelgard1162 <34759961+Edelgard1162@users.noreply.github.com>
2025-10-24 17:07:43 -0400

    Create pokedex.ts

---
+++ data/mods/gen4/pokedex.ts
@@ -0,0 +1,27 @@
+export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
+	milotic: {
+		inherit: true,
+		evoType: 'levelExtra',
+		evoCondition: 'with high Beauty',
+	},
+	rotomheat: {
+		inherit: true,
+		types: ["Electric", "Fire"],
+	},
+	rotomwash: {
+		inherit: true,
+		types: ["Electric", "Water"],
+	},
+	rotomfrost: {
+		inherit: true,
+		types: ["Electric", "Ice"],
+	},
+	rotomfan: {
+		inherit: true,
+		types: ["Electric", "Flying"],
+	},
+	rotommow: {
+		inherit: true,
+		types: ["Electric", "Grass"],
+	},
+};

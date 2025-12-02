c1e2e45e566ae0bcde63cf87a2969ed1f99083f0
Edelgard1162 <34759961+Edelgard1162@users.noreply.github.com>
2025-12-01 20:48:06 -0500

    Revert "Update dex-species.ts"
    This reverts commit f5374786fe254df5cb896c28200ab522ec0a1a0d.

--- sim/dex-species.ts
+++ sim/dex-species.ts
@@ -343,9 +343,9 @@ export class Species extends BasicEffect implements Readonly<BasicEffect & Speci
 		if (!this.gen && this.num >= 1) {
 			if (this.num >= 906 || this.forme.includes('Paldea')) {
 				this.gen = 9;
-			} else if (this.num >= 810 || ['Gmax'].includes(this.forme)) {
+			} else if (this.num >= 810 || ['Gmax', 'Galar', 'Galar-Zen', 'Hisui'].includes(this.forme)) {
 				this.gen = 8;
-			} else if (this.num >= 722) {
+			} else if (this.num >= 722 || this.forme.startsWith('Alola') || this.forme === 'Starter') {
 				this.gen = 7;
 			} else if (this.num >= 650 || this.isMega || this.isPrimal) {
 				this.gen = 6;
